24a1a44394e214a89e16838f0026cdfb1ab55bcb
AyoubZoggagh <58475728+AyoubZoggagh@users.noreply.github.com>
2020-03-28 22:00:14 +0100

    js toets

---
+++ overhoring.js
@@ -0,0 +1,108 @@
+      // Ayoub Zoggagh
+      // 4A9B
+      // Javascript
+
+
+
+       const cars = ['Volvo', 'Audi'];
+       console.log(cars.length);
+
+      const l = 2;
+      const m = 3;
+
+      console.log(l);     // verwachting : 2
+      
+
+          var hege = ["Ayoub", "Zoggagh"];
+              var stale = ["Muis", "Kat", "Rat"];
+                  var children = hege.concat(stale);
+
+          var str = "Hallo ik ben Ayoub";
+              var res = str.replace("Ayoub", "Zoggagh");
+
+                  var str = "Daar ben ik weer";
+          var res = str.substring(1, 4);
+              var str = "Overhoring jongen";
+                  var res = str.split(" ");
+
+                console.log(l = m + 1);
+                  console.log(l = l * m);
+                     console.log(l = l % m);
+
+               console.log(l = l / m);
+                  console.log(l = l - m);
+                      alert('Dit is een Alert!');
+              
+                 console.dir(document);
+                    console.log(document.getElementById('main'));
+                        const x = document.getElementById('main');
+                           const z = document.getElementById('hoi');
+
+
+
+          
+                 function bodyy(){
+                  document.body.style.backgroundColor = 'yellow';   // functie laat achtergrond kleur veranderen naar geel
+                  alert(typeof '' === 'string');
+                 }
+                 function ballie (){
+                     x.style.backgroundColor = 'blue';
+                     document.body.style.backgroundImage = "url('ziyech.jpg')";
+                      
+                 }
+
+                 function ja(){
+                     alert(Math.floor(Math.random() * 10)); // functie geeft alert random number onder de 10
+                 }
+
+
+                 function Ayoub(){
+                  document.body.style.backgroundImage = "url('ziyech1.jpg')"; // functie veranderd achtergrond foto naar andere foto
+                  alert(1 + 1 * 2 / 3 - 1 % 2); // alert geeft 5 Ass. operators
+                 }
+
+              const items = document.getElementsByClassName('list-group-item');
+
+          let num1 = 1;
+          let num2 = 3;
+
+
+          function rekenen(a=5, b=4) {    // functie met 2 parameters
+            const total = a + b;  // optellen
+            alert(total);      // resultaat wordt in alert gegeven
+          }
+          rekenen();
+
+              for(let i = 0; i < items.length; i++){    // for loop
+                      console.log('test' + i);
+                      const kleuren = ['paars','oranje','green','groen', 'rood', 'blauw', ' burleywood' ];  //array
+
+                      
+                  items[i].style.backgroundColor = kleuren[i];
+              items[i].textContent = 'Yo';
+              }
+
+              console.log(Math.random());
+
+              function color(){
+                 const  i = math.random();
+              }
+          const button = document.getElementById('nee');
+          function hi(){
+              document.getElementById('header-title').style.backgroundColor = 'white';
+          }
+
+          function format(){
+      
+          }
+
+           function buttonClick(){
+                console.log (' ingeklikt');
+             document.getElementById('main-header').textContent = 'Hahaha het werkt';
+              document.querySelector('#main').style.backgroundColor = 'red';
+
+              function jo(){
+                  document.getElementById('jo').style.backgroundColor = 'green';
+              }
+
+           }
